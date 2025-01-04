05b102b90fb6ccb11e12f7d79f439777ee92f828
Rohit <rohitvkgdg@gmail.com>
2025-01-04 20:57:57 +0530

    API key fix

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import { startUdpServer, createResponse, createTxtAnswer } from "denamed";
 import { GoogleGenerativeAI } from "@google/generative-ai"
+require('dotenv').config();
 
-const GEMINI_API_KEY = 'AIzaSyAVZlUit4dRnwoYEC9UQNd5YiMke9YMKnY';
+const API_KEY = process.env.GEMINI_API_KEY || '';
 
 startUdpServer(async (query): Promise<any> => {
     const question = query.questions ? query.questions[0] : null;
     const prompt = question ? question.name.split('.').join(' ') : '';
     // console.log(`Received query: ${prompt}`);
-    const ai = new GoogleGenerativeAI(GEMINI_API_KEY);
+    const ai = new GoogleGenerativeAI(API_KEY);
     const model = ai.getGenerativeModel({ model: 'gemini-1.5-flash-8b' });
     const result = await model.generateContent("Answer the question in a word or sentence: " + prompt);
     // console.log(`Response: ${result.response.text()}`);
